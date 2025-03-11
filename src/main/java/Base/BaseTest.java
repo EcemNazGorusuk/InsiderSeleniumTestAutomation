0266ebad69778d0fe7bf0588f268d5cf736558af
Ecem <ecemnazgorusuk@gmail.com>
2025-03-11 10:13:50 +0300

    base package updated

--- src/main/java/Base/BaseTest.java
+++ src/main/java/Base/BaseTest.java
@@ -1,4 +1,22 @@
 package Base;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
-public class BaseTest {
+public class BaseTest  extends BaseLibrary{
+   // MainPage mainPage =new MainPage();
+
+    @BeforeMethod
+    public void setUp() throws InterruptedException {
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.letgo.com/");
+        //mainPage.closeCokieNotificationButton();
+        //mainPage.clickKabulEtButton();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
 }
--- src/main/java/Base/Data.java
+++ src/main/java/Base/Data.java
@@ -1,4 +1,7 @@
 package Base;
 
 public class Data {
+   // public static String email="ecemnazgorusuk@gmail.com";
+   //public static String errMsgPassword="Şifren yanlış";
+
 }
